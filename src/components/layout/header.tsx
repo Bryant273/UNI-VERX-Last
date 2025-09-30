3ae41b70dabd1d500249348f3b0503676b9a9883
Ahmed Silué <silueahmed273@gmail.com>
2025-09-30 16:12:24 +0000

    JE trouve ces éléments trop à droite :
    - Barre de recherche
    - bouton cl

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -43,26 +43,26 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <Button
-          size="icon"
-          variant="ghost"
-          onClick={toggleSidebar}
-        >
-          <PanelLeft className="h-5 w-5" />
-          <span className="sr-only">Toggle Sidebar</span>
-        </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={toggleSidebar}
+        className="md:hidden"
+      >
+        <PanelLeft className="h-5 w-5" />
+        <span className="sr-only">Toggle Sidebar</span>
+      </Button>
 
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <DateTime />
-        <div className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                type="search"
-                placeholder="Search..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                />
-            </div>
+      <DateTime />
+
+      <div className="flex w-full items-center gap-4 md:gap-2 lg:gap-4 ml-auto">
+        <div className="relative ml-auto flex-1 md:grow-0">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search..."
+            className="w-full rounded-lg bg-muted pl-8 md:w-[200px] lg:w-[320px]"
+          />
         </div>
         <ThemeToggle />
         <Button variant="ghost" size="icon" aria-label="Notifications">
