ec9b40aa9a37d140decae7bcc8f5ab9c927281f7
Ahmed Silué <silueahmed273@gmail.com>
2025-12-16 22:57:22 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/admin/planning/page.tsx
+++ src/app/admin/planning/page.tsx
@@ -1,5 +1,6 @@
+
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
